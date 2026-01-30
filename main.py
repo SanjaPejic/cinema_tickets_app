3f12525c0b549c9a9912edfafaf13fb8f0c21414
Sanja <sanja.pejic17@gmail.com>
2026-01-30 14:46:37 +0100

    [SP-2] fix: adjust register validation

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def load_data() -> []:
             data_matrix.append(row)
 
     except FileNotFoundError as e:
-        print(f"File is not found, {e} ")
+        print(f"File not found: {e} ")
 
     return data_matrix
 
@@ -34,8 +34,8 @@ def check_existing_user(username: str, password: str) -> User | None:
             try:
                 role = Role(row[4])
             except ValueError:
-                print("Role is not properly written in the data file")
-                print("Presumed role is customer by default")
+                print("Invalid role in the data file.")
+                print("Defaulting to customer.")
                 role = Role.CUSTOMER
             return User(username,password, row[2], row[3], role)
     return None
@@ -43,19 +43,20 @@ def check_existing_user(username: str, password: str) -> User | None:
 
 def login() -> User:
     while True:
-        username = input("\nInput Username: ").strip()
-        password = input("Input password: ").strip()
+        username = input("\nEnter Username: ").strip()
+        password = input("Enter password: ").strip()
         found_user = check_existing_user(username, password)
 
         if found_user is not None:
             break
-        print("Wrong username or password. Try again.")
+        print("Invalid login. Try again.")
 
-    print(f"{username}, successful login")
+    print(f"Login successful, {username}")
     return found_user
 
 
 def register() -> User | None:
+
     unique = False
     while not unique:
         username = input("Create a username: ")
@@ -64,25 +65,36 @@ def register() -> User | None:
         for line in data_matrix:
             if line[0] == username:
                 unique = False
-                print("Username already exists. Try again.")
+                print("The username already exists. Try again.")
 
     password = ""
-    while len(password) < 7:
-        password = input("Create a password (min 7 characters): ")
+    has_digit = False
+    is_long = False
+    while not (has_digit and is_long):
+        password = input("Create a password: ")
+        if len(password) >= 7:
+            is_long = True
+        else:
+            print("Use at least 7 characters. Try again.")
+        for char in password:
+            if char.isdigit():
+                has_digit = True
+        if not has_digit:
+            print("Include at least one digit. Try again.")
 
-    name = input("Input name: ")
-    surname = input("Input surname: ")
+    name = input("Enter your first name: ")
+    surname = input("Enter your surname: ")
 
     try:
         with open(data_file_path, mode="a+") as data_file:
             data_file.write(f"\n{username}|{password}|{name}|{surname}|{Role.CUSTOMER}")
             new_user = User(username, password, name, surname, Role.CUSTOMER)
-            print("You are assigned a role of a customer.")
-            print(f"{username} is successfully created.")
+            print("Assigned role: customer.")
+            print(f"Account created: {username}.")
             return new_user
 
     except FileNotFoundError as e:
-        print(f"File is not found, {e} ")
+        print(f"File not found, {e} ")
 
     return None
 
@@ -94,7 +106,7 @@ def display_auth_menu() -> User:
         print("2. Register")
         print("3. Exit")
         print("---------------------------------------")
-        choice = input("Choose 1, 2 or 3: ").strip()
+        choice = input("Choose 1, 2, or 3: ").strip()
 
         if choice == "1":
             return login()
@@ -104,12 +116,12 @@ def display_auth_menu() -> User:
             print("\nExiting the program...")
             sys.exit()
         else:
-            print("\nInvalid choice. Enter number 1 or number 2 or 3.")
+            print("\nInvalid choice. Enter 1, 2, or 3.")
 
 
 def display_user_menu(user: User):
     print("---------------------------------------------------------------------------")
-    print(f"Main Menu {user.username}")
+    print(f"Main Menu: {user.username}")
     print("1. ")
     print("2. ")
     print("3. ")
@@ -120,6 +132,6 @@ def display_user_menu(user: User):
 
 if __name__ == '__main__':
     logged_user = display_auth_menu()
-    print(f"\nWelcome {logged_user.name} {logged_user.surname}!")
+    print(f"\nWelcome, {logged_user.name} {logged_user.surname}!")
     display_user_menu(logged_user)
 
